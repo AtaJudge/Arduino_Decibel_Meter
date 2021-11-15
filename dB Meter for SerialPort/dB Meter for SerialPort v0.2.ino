76c3fc29f4a9428dce2613c55253fc6fb054f0d2
Ata Berke YARGIÇ <73077119+AtaJudge@users.noreply.github.com>
2021-11-15 22:59:10 +0300

    Add files via upload

--- dB Meter for SerialPort/dB Meter for SerialPort v0.2.ino
+++ dB Meter for SerialPort/dB Meter for SerialPort v0.2.ino
@@ -1,48 +1,49 @@
- ​#​define​ ​led​ ​3 
- ​#​define​ ​maxAmp​ (​512​) 
- ​#​define​ ​dBPinNum​ ​0
- 
- int RefdBarv;
-  
- ​String warningstr =   ​"​dB. Kabul edilebilir değerin üstünde.​"​; 
- ​String standardstr =  ​"​dB. Kabul edilebilir değer aralığında.​"​; 
-  
- ​void​ ​setup​() 
- ​{ 
- ​  ​pinMode​(led, OUTPUT); 
- ​  ​pinMode​(dBPinNum, INPUT); 
- ​  Serial.​begin​(​9600​); 
- ​} 
-  
- ​void​ ​loop​() 
- ​{ 
- ​  ​float​ dBval=​DecibelMeter​(); 
- ​  ​if​(dBval>​70​) 
- ​  { 
- ​    Serial.​println​(dBval+warningstr); 
- ​    ​digitalWrite​(led, HIGH); 
- ​  } 
- ​  ​else​ ​if​(dBval<=​70​) 
- ​  { 
- ​    Serial.​println​(dBval+standardstr); 
- ​    ​digitalWrite​(led, LOW); 
- ​  } 
- ​} 
-  
- ​float​ ​DecibelMeter​()  
- ​{
-   float​ decibel = ​0​;
-   int ARV = 0;
-   
-   ARV = analogRead(dBPinNum);
- ​  decibel = ​(ARV / ​refdB​arv); 
- ​  ​double​ dB = 20​*(​log10​(decibel))+X; //Replace X with your reference dB value.
- ​  ​return​ dB; 
- ​}
- 
-/*void calibration ()
- {
-   //play sound at a known dB level. Let this value be X dB.
-   refdBarv = analogRead(dBPinNum); //This is the value of X dB in ARV.   
- }*/
- 
+ #define led 3
+ #define maxAmp 512
+ #define dBPinNum 0
+ #define X 80
+
+int Ref_dB_arv;
+
+String warningstr="dB. Out of the acceptable range.";
+String standartstr="dB. Within the acceptable range.";
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  pinMode(dBPinNum, INPUT);
+}
+
+void loop()
+{
+  float dBval=DecibelMeter();
+  if(dBval>70)
+  {
+    Serial.println(dBval+warningstr);
+    digitalWrite(led, HIGH); 
+  }
+  else if(dBval<=70)
+  {
+    Serial.println(dBval+standartstr);
+    digitalWrite(led, LOW);
+  }
+}
+
+float DecibelMeter()
+{
+  float decibel = 0;
+  int ARV = 0;
+
+  ARV=analogRead(dBPinNum);
+  decibel=(ARV/Ref_dB_arv);
+  double dB = 20*(log10(decibel))+X; //Replace X with your referance dB value.
+  return dB;
+}
+
+/*
+void calibration()
+{
+  //Play sound at known dB level. Let this value be X dB.
+  Ref_dB_arv = analogRead(dBPinNum); //This is the value of X dB in analog (0-1023) value.
+}
+*/
